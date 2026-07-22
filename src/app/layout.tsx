7a9c1549077f8604ac2af1bab4b4dc5bb9b47076
elixirentertainmentltd-cyber <elixirentertainmentltd@gmail.com>
2026-07-22 19:27:18 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,13 @@ import './part5.css';
 export const metadata: Metadata = {
   title: 'We Are Elixir Academy',
   description: 'Learning and development for the We Are Elixir community',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/icon.png', type: 'image/png', sizes: '512x512' },
+    ],
+    apple: [{ url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
